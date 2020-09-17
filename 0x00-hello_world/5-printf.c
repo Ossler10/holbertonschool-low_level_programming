399f5a189b54aa69a9f038e23f023bc6adc117cb
Ossler10 <2443@holbertonschool.com>
2020-09-16 21:12:33 -0500

    Version 1.0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,6 @@
+julien@ubuntu:~/c/0x00$ gcc -Wall 5-printf.c
+julien@ubuntu:~/c/0x00$ ./a.out 
+with proper grammar, but the outcome is a piece of art,
+julien@ubuntu:~/c/0x00$ echo $?
+0
+julien@ubuntu:~/c/0x00$ 
